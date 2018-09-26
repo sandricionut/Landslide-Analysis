5549d3e9493dfc0a22362885f860bdc2e0e3bef8
Ionut Sandric <sandricionut@yahoo.com>
2018-09-26 16:30:41 +0300

    Bug fix for unexpected iterative simulations

--- run/runAnalysisWofE.py
+++ run/runAnalysisWofE.py
@@ -39,13 +39,15 @@ if __name__ == "__main__":
     for noise_value in noise_values:
         # mask_raster = dem_simulate.CreateMask(mask_vector, "ID", cell_size=arcpy.Raster(in_dem).meanCellHeight)
         # noise_order_value=3
+        print("Noise: ", noise_value)
         out_stats_file = os.path.join(out_path_stats, "prodCondI2018_{}.txt".format(noise_value))
         dem_simulate = demSimulation.DemSimulations(in_dem=indem, out_dem=r"d:\Personal\Temp\Temporar\demsim.tif", mean=landslides_mapping_error_mean, stdev=landslides_mapping_error_stdev, noise_order=noise_value, temp_location=out_path_temp, mask=mask_raster)
         # dem_simulate.run()
         landslide_simulate = landslidesSimulation.LandslideSimulations(in_landslides=initial_landslides, out_landslides=r"d:\Personal\Temp\Temporar\ls.shp", mean=landslides_mapping_error_mean, stdev=landslides_mapping_error_stdev, temp_location=out_path_temp)
 
         wofe_m = woefSimulation.WofESimulations(out_woe_map=out_path_woe, out_name_prefix="wf_{}".format(str(noise_value)))
-        wofe_m.simulate(2, in_dem_sim=dem_simulate, landslide_sim=landslide_simulate, stats_file=out_stats_file, nominal_data=[inlulc, inlithology], mask=mask_raster, noise_factor=noise_value)
+        for i in range(0, 100, 1):
+            wofe_m.simulate(i, in_dem_sim=dem_simulate, landslide_sim=landslide_simulate, stats_file=out_stats_file, nominal_data=[inlulc, inlithology], mask=mask_raster, noise_factor=noise_value)
 
 
     # for file in os.listdir(out_path_temp):
--- simulations/woefSimulation.py
+++ simulations/woefSimulation.py
@@ -8,7 +8,7 @@ from analysis import tacuda
 from analysis import randomSelection
 from analysis import wofe
 from sklearn.metrics import roc_auc_score
-
+import datetime
 
 class WofESimulations():
 
@@ -20,83 +20,90 @@ class WofESimulations():
     def simulate(self, no_simulations, in_dem_sim, landslide_sim, stats_file, nominal_data, mask, noise_factor):
         # Generate noise for DEM
         # terrain_analysis = tacuda.taCUDA(georsgpu_path=georsgpu)
+        start = datetime.datetime.now()
+        i = no_simulations
         georsgpu = r"d:\GitHub\GeoRsGPU\GeoRsGPU\x64\Release\GeoRsGPU.exe"
         ta = tacuda.taCUDA(georsgpu_path=georsgpu)
         we = wofe.WofE()
         rs = randomSelection.RandomSelection()
         temp_location = in_dem_sim.get_temp_location()
-        for i in range(0, no_simulations, 1):
-            array_evidence = []
-            # array_evidence.append(x for x in nominal_data)
-            array_evidence = nominal_data
-            dem_noise = in_dem_sim.dem_noise2(noise_factor=noise_factor)
-            landslide_noise = landslide_sim.simulate()
-            print()
-            #
-            # Terrain analyses
-            outSlope = os.path.join(temp_location, "slope{}.tif".format(str(i)))
-            outProfileCurvature = os.path.join(temp_location, "profc{}.tif".format(str(i)))
-            out_plan_curvature = os.path.join(temp_location, "planc{}.tif".format(str(i)))
+        # for i in range(0, no_simulations, 1):
+        print("Simulare: ", i)
+        array_evidence = []
+        # array_evidence.append(x for x in nominal_data)
+        array_evidence = nominal_data
+        dem_noise = in_dem_sim.dem_noise2(noise_factor=noise_factor)
+        landslide_noise = landslide_sim.simulate()
+        # print()
+        #
+        # Terrain analyses
+        outSlope = os.path.join(temp_location, "slope{}.tif".format(str(i)))
+        outProfileCurvature = os.path.join(temp_location, "profc{}.tif".format(str(i)))
+        out_plan_curvature = os.path.join(temp_location, "planc{}.tif".format(str(i)))
 
-            # Reclassified rasters
-            outSlopeR = os.path.join(temp_location, "slopeR_{}.tif".format(str(i)))
-            out_profile_curvatureR = os.path.join(temp_location, "profcR_{}.tif".format(str(i)))
-            out_plan_curvatureR = os.path.join(temp_location, "plancR_{}.tif".format(str(i)))
+        # Reclassified rasters
+        outSlopeR = os.path.join(temp_location, "slopeR_{}.tif".format(str(i)))
+        out_profile_curvatureR = os.path.join(temp_location, "profcR_{}.tif".format(str(i)))
+        out_plan_curvatureR = os.path.join(temp_location, "plancR_{}.tif".format(str(i)))
 
-            # Call georsgpu for terrain analyses - this can be replaced with any other terrain analysis software
-            ta.slope(dem_noise, outSlope)
-            ta.plan_curvature(dem_noise, outProfileCurvature)
-            ta.profile_curvature(dem_noise, out_plan_curvature)
+        # Call georsgpu for terrain analyses - this can be replaced with any other terrain analysis software
+        ta.slope(dem_noise, outSlope)
+        ta.plan_curvature(dem_noise, outProfileCurvature)
+        ta.profile_curvature(dem_noise, out_plan_curvature)
 
-            # Evidence classification
-            classesCurvature = numpy.arange(-3.0, 3.5, 0.5)
-            classesSlope = numpy.arange(0, 90, 3)
-            reclassCurvature = []
-            reclassSlope = []
+        # Evidence classification
+        classesCurvature = numpy.arange(-3.0, 3.5, 0.5)
+        classesSlope = numpy.arange(0, 90, 3)
+        reclassCurvature = []
+        reclassSlope = []
 
 
-            # Add minimum curvature value - estimated
-            reclassCurvature.append([-100, -3.0, 1])
-            for clasa in range(1, len(classesCurvature)):
-                reclassCurvature.append([round(classesCurvature[clasa - 1], 4), round(classesCurvature[clasa], 4), clasa + 1])
-            for clasa in range(1, len(classesSlope)):
-                reclassSlope.append([round(classesSlope[clasa - 1], 4), round(classesSlope[clasa], 4), clasa])
-            # Add maximum curvature value - estimated
-            reclassCurvature.append([3.0, 100, len(classesCurvature) + 1])
+        # Add minimum curvature value - estimated
+        reclassCurvature.append([-100, -3.0, 1])
+        for clasa in range(1, len(classesCurvature)):
+            reclassCurvature.append([round(classesCurvature[clasa - 1], 4), round(classesCurvature[clasa], 4), clasa + 1])
+        for clasa in range(1, len(classesSlope)):
+            reclassSlope.append([round(classesSlope[clasa - 1], 4), round(classesSlope[clasa], 4), clasa])
+        # Add maximum curvature value - estimated
+        reclassCurvature.append([3.0, 100, len(classesCurvature) + 1])
 
-            outSlopeReclass = arcpy.sa.Reclassify(outSlope, "Value", arcpy.sa.RemapRange(reclassSlope), missing_values="NODATA") * mask
-            outSlopeReclass.save(outSlopeR)
+        outSlopeReclass = arcpy.sa.Reclassify(outSlope, "Value", arcpy.sa.RemapRange(reclassSlope), missing_values="NODATA") * mask
+        outSlopeReclass.save(outSlopeR)
 
-            outProfCurvatureReclass = arcpy.sa.Reclassify(outProfileCurvature, "Value", arcpy.sa.RemapRange(reclassCurvature), missing_values="NODATA") * mask
-            outProfCurvatureReclass.save(out_plan_curvatureR)
+        outProfCurvatureReclass = arcpy.sa.Reclassify(outProfileCurvature, "Value", arcpy.sa.RemapRange(reclassCurvature), missing_values="NODATA") * mask
+        outProfCurvatureReclass.save(out_plan_curvatureR)
 
-            out_plan_curvatureReclass = arcpy.sa.Reclassify(out_plan_curvature, "Value", arcpy.sa.RemapRange(reclassCurvature), missing_values="NODATA") * mask
-            out_plan_curvatureReclass.save(out_profile_curvatureR)
+        out_plan_curvatureReclass = arcpy.sa.Reclassify(out_plan_curvature, "Value", arcpy.sa.RemapRange(reclassCurvature), missing_values="NODATA") * mask
+        out_plan_curvatureReclass.save(out_profile_curvatureR)
 
-            array_evidence.append(outSlopeR)
-            array_evidence.append(out_profile_curvatureR)
-            array_evidence.append(out_plan_curvatureR)
+        array_evidence.append(outSlopeR)
+        array_evidence.append(out_profile_curvatureR)
+        array_evidence.append(out_plan_curvatureR)
 
-            sel_landslides_train = os.path.join(temp_location, "ssltrain{}.shp".format(i))
-            sel_landslides_test = os.path.join(temp_location, "ssltest{}.shp".format(i))
-            sim_landslides_raster_train = os.path.join(temp_location, "ssltrain{}.tif".format(i))
-            sim_landslides_raster_test = os.path.join(temp_location, "ssltest{}.tif".format(i))
+        sel_landslides_train = os.path.join(temp_location, "ssltrain{}.shp".format(i))
+        sel_landslides_test = os.path.join(temp_location, "ssltest{}.shp".format(i))
+        sim_landslides_raster_train = os.path.join(temp_location, "ssltrain{}.tif".format(i))
+        sim_landslides_raster_test = os.path.join(temp_location, "ssltest{}.tif".format(i))
 
-            rs.random_select2(input_file=landslide_noise, out_file_train=sel_landslides_train, out_file_test=sel_landslides_test, train_size=70)
+        rs.random_select2(input_file=landslide_noise, out_file_train=sel_landslides_train, out_file_test=sel_landslides_test, train_size=70)
 
-            in_landslides_train = landslide_sim.landslide2raster(in_landslides=sel_landslides_train, out_landslide=sim_landslides_raster_train, reference_raster=arcpy.Raster(dem_noise), mask=mask)
-            in_landslides_test = landslide_sim.landslide2raster(in_landslides=sel_landslides_test, out_landslide=sim_landslides_raster_test, reference_raster=arcpy.Raster(dem_noise), mask=mask)
-            # in_landslides.save(os.path.join(temp_location, "lsim{}.tif".format(i)))
-            #
-            priorProb = we.priorProbability(in_landslides_train)
-            #
-            rastersResult = we.conditionalProbability(in_landslides_train, array_evidence, stats_file, iteration=i)
-            #
-            we.woeMap(rastersResult, arcpy.Raster(dem_noise), os.path.join(self._out_woe_map, "{}_{}.tif".format(self._out_name_prefix, str(i))))
+        in_landslides_train = landslide_sim.landslide2raster(in_landslides=sel_landslides_train, out_landslide=sim_landslides_raster_train, reference_raster=arcpy.Raster(dem_noise), mask=mask)
+        in_landslides_test = landslide_sim.landslide2raster(in_landslides=sel_landslides_test, out_landslide=sim_landslides_raster_test, reference_raster=arcpy.Raster(dem_noise), mask=mask)
+        # in_landslides.save(os.path.join(temp_location, "lsim{}.tif".format(i)))
+        #
+        priorProb = we.priorProbability(in_landslides_train)
+        #
+        rastersResult = we.conditionalProbability(in_landslides_train, array_evidence, stats_file, iteration=i)
+        #
+        we.woeMap(rastersResult, arcpy.Raster(dem_noise), os.path.join(self._out_woe_map, "{}_{}.tif".format(self._out_name_prefix, str(i))))
+
+        stop = datetime.datetime.now()
+
+        print("Timp simulare: ", stop - start)
 
 #             Validation ROC-AUC
 #             n_sim_landslides_raster_test = arcpy.RasterToNumPyArray(sim_landslides_raster_test)
-            # n_raster_result = arcpy.RasterToNumPyArray(os.path.join(self._out_woe_map, "wf{}.tif".format(str(i))))
-            # roc_auc = roc_auc_score(n_sim_landslides_raster_test, n_raster_result)
-            # print(roc_auc)
+        # n_raster_result = arcpy.RasterToNumPyArray(os.path.join(self._out_woe_map, "wf{}.tif".format(str(i))))
+        # roc_auc = roc_auc_score(n_sim_landslides_raster_test, n_raster_result)
+        # print(roc_auc)
 
